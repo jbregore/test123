26031799fc737eab6cee1ad4288e7c0f1875c408
jbregore <jbbbregore099@gmail.com>
2020-12-13 18:17:51 +0800

    update bagya

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ function menuToggle() {
 		menuItems.style.maxHeight = "0px";
 	}
 }
+ 
+//action.html toggle form
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+	RegForm.style.transform = " translateX(0px) ";
+	LoginForm.style.transform = " translateX(0px) ";
+	Indicator.style.transform = " translateX(100px) ";
+}
+
+function login() {
+	RegForm.style.transform = " translateX(400px) ";
+	LoginForm.style.transform = " translateX(400px) ";
+	Indicator.style.transform = " translateX(0px) ";
+}
+
